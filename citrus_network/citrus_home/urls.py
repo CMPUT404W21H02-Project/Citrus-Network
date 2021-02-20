2bcfd2effe6a4f8d1c6dfa57071c936be9935ede
fujishig2 <fujishig@ualberta.ca>
2021-02-20 16:22:34 -0700

    added nav bar

--- citrus_network/citrus_home/urls.py
+++ citrus_network/citrus_home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.login_redirect),
-  path('home/', views.home_redirect),
-  path('register/', views.register_redirect),
+  path('', views.login_redirect, name='login_url'),
+  path('home/', views.home_redirect, name='home_url'),
+  path('register/', views.register_redirect, name='register_url'),
 ]
