1d2e71bca94ec76e4347ee40998ed532bbdcede7
Nhan <nhnguyen@ualberta.ca>
2021-02-19 17:49:13 -0700

    added profile edit template and func

--- citrus_network/citrus_home/urls.py
+++ citrus_network/citrus_home/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('', views.index, name='index'),
   path('home/', views.home_redirect),
   path('test', views.test_sign_up),
-  path('service/author/<uuid:id>/',views.fetch_profile, name='profile'),
+  path('service/author/<uuid:id>/',views.get_profile, name='profile'),
+  path('service/author/<uuid:id>/edit',views.post_profile, name='edit'),
 ]
--- citrus_network/citrus_home/views.py
+++ citrus_network/citrus_home/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from django.contrib.auth import authenticate
 from django.http.response import JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from http import HTTPStatus
 
 def index(request):
@@ -68,8 +69,41 @@ def test_sign_up(request):
 
 
 """
-handles get requests and get profile information: username, displayname, github 
+handles get requests with author_id and retrieve author profile information: username, displayname, github 
+URL:/service/author/{AUTHOR_ID}
 """
-def fetch_profile(request, id):
+def get_profile(request, id):
     profile = get_object_or_404(CitrusUser, id=id)
     return render(request, 'citrus_home/profile.html',{'user': profile})
+
+"""
+handles post requests with author_id and state changes to author profile information: username, displayname, github 
+URL:/service/author/{AUTHOR_ID}
+"""
+def post_profile(request, id):
+    profile = get_object_or_404(CitrusUser, id=id)
+
+    # NEED TO SANITIZE DATA AND CHECK FOR UNCHANGED INPUT
+
+    # try:
+    #     new_profile = profile.choice_set.get(id=request.POST['choice'])
+    # except (KeyError, CitrusUser.DoesNotExist):
+    #     # Redisplay the question voting form.
+    #     return render(request, 'citrus_home/profile.html', {
+    #         'question': profile,
+    #         'error_message': "Something went wrong.",
+    #     })
+    # else:
+    #     profile.votes += 1
+    #     selected_choice.save()
+    #     # Always return an HttpResponseRedirect after successfully dealing
+    #     # with POST data. This prevents data from being posted twice if a
+    #     # user hits the Back button.
+    #     return HttpResponseRedirect(reverse('citrus_home/profile.html', args=(id))) # 'polls:results'
+    
+    return HttpResponseRedirect(reverse('profile', args=(profile.id,))) # 'polls:results'
+
+    # profile = get_object_or_404(CitrusUser, id=id)
+    
+    # return render(request, 'citrus_home/profile.html',{'user': profile})
+
