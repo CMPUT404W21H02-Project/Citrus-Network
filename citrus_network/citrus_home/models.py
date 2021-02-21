4a8b83018825f3b5e5adfbac52dd81dcdee54c52
Nhan <nhnguyen@ualberta.ca>
2021-02-21 00:02:55 -0700

    updated model

--- citrus_network/citrus_home/models.py
+++ citrus_network/citrus_home/models.py
@@ -4,9 +4,9 @@ from django.contrib.auth.models import User
 
 
 class CitrusAuthor(models.Model):
-    user_type       = models.CharField(max_length=100, default="Author")
-    author_id       = models.CharField(max_length=50)
+    type            = models.CharField(max_length=100, default="Author")
+    id              = models.CharField(max_length=50,primary_key=True)
     user            = models.OneToOneField(User, on_delete=models.CASCADE)
     host            = models.CharField(max_length=200, default="http://localhost:8000/")
-    display_name    = models.CharField(max_length=300, default=f"{str(user)}")
+    displayName     = models.CharField(max_length=300, default=f"{str(user)}")
     github          = models.CharField(max_length=300, default="", null=True)
--- citrus_network/citrus_home/views.py
+++ citrus_network/citrus_home/views.py
@@ -19,7 +19,6 @@ import uuid
 def home_redirect(request):
     return render(request, 'citrus_home/index.html')
 
-
 def login_redirect(request):
     if request.method == "POST":
         print("here")
@@ -53,7 +52,7 @@ def register_redirect(request):
             username = request.POST.get('username')
             password = request.POST.get('password')
             # create CitrusAuthor
-            citrusAuthor = CitrusAuthor.objects.create(user_type="author",author_id=str(uuid.uuid4()), user=user,display_name=str(user.username))
+            citrusAuthor = CitrusAuthor.objects.create(type="author",id=str(uuid.uuid4()), user=user,displayName=user.username)
             citrusAuthor.save()
             return redirect(home_redirect) 
     
@@ -63,43 +62,16 @@ def register_redirect(request):
     return render(request, 'citrus_home/register.html', {'form': form})
 
 """
-handles post requests and checks for a username and password, if the username is not taken then a citrus user is created.
-Expected: POST - POST body = {"username": "some_usrname", "password": "some_password"}
-"""
-@csrf_exempt 
-def test_sign_up(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        try:
-            user = CitrusUser.objects.create_user(username=username, password=password)
-            user.save()
-            response = JsonResponse({
-                "message": "user created!"
-            })
-            response.status_code = 200
-            return response
-        except:
-            print('inside except')
-            response = JsonResponse({
-                "message": "citrus user not created username probably taken"
-            })
-            # TODO: find status code
-            response.status_code = 418
-            return response
-
-
-"""
-handles get requests with author_id and retrieve author profile information: username, displayname, github
+handles get requests with id and retrieve author profile information: username, displayname, github
 handles post requests to state changes to author profile information: username, displayname, github 
 Expected: POST - POST body = {"username": "new_username", "displayName": "new_displayName", "github":"new_github"} 
 URL:/service/author/{AUTHOR_ID}
 """
 def manage_profile(request, id):
     if request.method == 'GET':
-        profile = get_object_or_404(CitrusAuthor, author_id=id)
+        profile = get_object_or_404(CitrusAuthor, id=id)
         current_profile = { 'username': profile.user,
-                            'displayName': profile.display_name,
+                            'displayName': profile.displayName,
                             'github': profile.github}
         form = ProfileForm(current_profile)
 
@@ -113,9 +85,9 @@ def manage_profile(request, id):
         new_github = request.POST.get('github')
 
         try:
-            profile = get_object_or_404(CitrusAuthor, author_id=id)
+            profile = get_object_or_404(CitrusAuthor, id=id)
             profile.user.username = new_username
-            profile.display_name = new_displayName
+            profile.displayName = new_displayName
             profile.github = new_github
             profile.user.save()
             profile.save()
@@ -124,7 +96,7 @@ def manage_profile(request, id):
                 "message": "profile updated!"
             })
             response.status_code = 200
-            return HttpResponseRedirect(reverse('profile',  kwargs={ 'id': str(profile.author_id) }))
+            return HttpResponseRedirect(reverse('profile',  kwargs={ 'id': str(profile.id) }))
 
         except (KeyError, CitrusAuthor.DoesNotExist):
             response = JsonResponse({
@@ -140,43 +112,3 @@ def manage_profile(request, id):
 
         response.status_code = 405
         return response
-
-# def post_profile(request,id):
-#     # if this is a POST request we need to process the form data
-#     if request.method == 'POST':
-#         # NEED TO SANITIZE DATA AND CHECK FOR UNCHANGED INPUT
-#         new_username = request.POST.get('username')
-#         new_displayName = request.POST.get('displayName')
-#         new_github = request.POST.get('github')
-
-#         print(new_username,new_displayName,new_github)
-
-#         try:
-#             profile = get_object_or_404(CitrusUser, id=id)
-#             profile.username = new_username
-#             profile.displayName = new_displayName
-#             profile.github = new_github
-
-#             profile.save()
-
-#             response = JsonResponse({
-#                 "message": "profile updated!"
-#             })
-#             response.status_code = 200
-#             return HttpResponseRedirect(reverse('profile',  kwargs={ 'id': str(profile.id) }))
-
-#         except (KeyError, CitrusUser.DoesNotExist):
-#             # Redisplay the question voting form.
-#             response = JsonResponse({
-#                 "message": "couldn't update profile"
-#             })
-#             # TODO: find status code
-#             response.status_code = 418
-#             return response
-#     else:
-#         profile = get_object_or_404(CitrusUser, id=id)
-#         current_profile = { 'username': profile.username,
-#                             'displayName': profile.displayName,
-#                             'github': profile.github}
-#         form = ProfileForm(current_profile)
-#     return render(request, 'citrus_home/edit_profile.html', {'form': form, 'user': profile})
