c998b3793821fb726546d3ac42244fbd9b7bd160
Nhan <nhnguyen@ualberta.ca>
2021-02-20 23:20:29 -0700

    added manage profile API

--- citrus_network/citrus_home/urls.py
+++ citrus_network/citrus_home/urls.py
@@ -3,8 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',views.get_profile, name='profile'),
-  path('service/author/<uuid:id>/edit/',views.post_profile, name='profile_edit'),
+  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',views.manage_profile, name='profile'),
   path('', views.login_redirect, name='login_url'),
   path('home/', views.home_redirect, name='home_url'),
   path('register/', views.register_redirect, name='register_url'),
--- citrus_network/citrus_home/views.py
+++ citrus_network/citrus_home/views.py
@@ -91,58 +91,92 @@ def test_sign_up(request):
 
 """
 handles get requests with author_id and retrieve author profile information: username, displayname, github
-if click edit button, redirect to edit profile page 
+handles post requests to state changes to author profile information: username, displayname, github 
+Expected: POST - POST body = {"username": "new_username", "displayName": "new_displayName", "github":"new_github"} 
 URL:/service/author/{AUTHOR_ID}
 """
-def get_profile(request, id):
+def manage_profile(request, id):
     if request.method == 'GET':
-        profile = get_object_or_404(CitrusUser, id=id)
-        return render(request, 'citrus_home/profile.html',{'user': profile})
-    else:
-        return HttpResponseRedirect(reverse('profile_edit',  kwargs={ 'id': str(id) }))
+        profile = get_object_or_404(CitrusAuthor, author_id=id)
+        current_profile = { 'username': profile.user,
+                            'displayName': profile.display_name,
+                            'github': profile.github}
+        form = ProfileForm(current_profile)
 
-
-"""
-handles state changes to author profile information: username, displayname, github and redirect back to profile page
-URL:/service/author/{AUTHOR_ID}/edit
-Expected: POST - POST body = {"username": "new_username", "displayName": "new_displayName", "github":"new_github"}
-"""
-def post_profile(request,id):
+        return render(request, 'citrus_home/profile.html',{'form': form, 'user': profile})
+    
     # if this is a POST request we need to process the form data
-    if request.method == 'POST':
+    elif request.method == 'POST':
         # NEED TO SANITIZE DATA AND CHECK FOR UNCHANGED INPUT
         new_username = request.POST.get('username')
         new_displayName = request.POST.get('displayName')
         new_github = request.POST.get('github')
 
-        print(new_username,new_displayName,new_github)
-
         try:
-            profile = get_object_or_404(CitrusUser, id=id)
-            profile.username = new_username
-            profile.displayName = new_displayName
+            profile = get_object_or_404(CitrusAuthor, author_id=id)
+            profile.user.username = new_username
+            profile.display_name = new_displayName
             profile.github = new_github
-
+            profile.user.save()
             profile.save()
 
             response = JsonResponse({
                 "message": "profile updated!"
             })
             response.status_code = 200
-            return HttpResponseRedirect(reverse('profile',  kwargs={ 'id': str(profile.id) }))
+            return HttpResponseRedirect(reverse('profile',  kwargs={ 'id': str(profile.author_id) }))
 
-        except (KeyError, CitrusUser.DoesNotExist):
-            # Redisplay the question voting form.
+        except (KeyError, CitrusAuthor.DoesNotExist):
             response = JsonResponse({
                 "message": "couldn't update profile"
             })
-            # TODO: find status code
             response.status_code = 418
             return response
+    #not POST AND GET SO return sth else 
     else:
-        profile = get_object_or_404(CitrusUser, id=id)
-        current_profile = { 'username': profile.username,
-                            'displayName': profile.displayName,
-                            'github': profile.github}
-        form = ProfileForm(current_profile)
-    return render(request, 'citrus_home/edit_profile.html', {'form': form, 'user': profile})
+        response = JsonResponse({
+            "message": "Method Not Allowed. Only support GET and POST"
+        })
+
+        response.status_code = 405
+        return response
+
+# def post_profile(request,id):
+#     # if this is a POST request we need to process the form data
+#     if request.method == 'POST':
+#         # NEED TO SANITIZE DATA AND CHECK FOR UNCHANGED INPUT
+#         new_username = request.POST.get('username')
+#         new_displayName = request.POST.get('displayName')
+#         new_github = request.POST.get('github')
+
+#         print(new_username,new_displayName,new_github)
+
+#         try:
+#             profile = get_object_or_404(CitrusUser, id=id)
+#             profile.username = new_username
+#             profile.displayName = new_displayName
+#             profile.github = new_github
+
+#             profile.save()
+
+#             response = JsonResponse({
+#                 "message": "profile updated!"
+#             })
+#             response.status_code = 200
+#             return HttpResponseRedirect(reverse('profile',  kwargs={ 'id': str(profile.id) }))
+
+#         except (KeyError, CitrusUser.DoesNotExist):
+#             # Redisplay the question voting form.
+#             response = JsonResponse({
+#                 "message": "couldn't update profile"
+#             })
+#             # TODO: find status code
+#             response.status_code = 418
+#             return response
+#     else:
+#         profile = get_object_or_404(CitrusUser, id=id)
+#         current_profile = { 'username': profile.username,
+#                             'displayName': profile.displayName,
+#                             'github': profile.github}
+#         form = ProfileForm(current_profile)
+#     return render(request, 'citrus_home/edit_profile.html', {'form': form, 'user': profile})
