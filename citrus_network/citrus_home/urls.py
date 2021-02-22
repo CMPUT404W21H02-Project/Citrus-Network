36e6af8c16b1ce6a77639dc12d92ada16ebee4f3
Nhan <nhnguyen@ualberta.ca>
2021-02-22 03:10:07 -0700

    added github activity api

--- citrus_network/citrus_home/urls.py
+++ citrus_network/citrus_home/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
   url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',views.manage_profile, name='profile'),
+  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/github$',views.get_github_events, name='github'),
   path('', views.login_redirect, name='login_url'),
   path('home/', views.home_redirect, name='home_url'),
   path('register/', views.register_redirect, name='register_url'),
--- citrus_network/citrus_home/views.py
+++ citrus_network/citrus_home/views.py
@@ -15,6 +15,8 @@ from .profile_form import ProfileForm
 from django.urls import reverse
 from django.contrib.auth.models import User
 import uuid
+import requests
+import re
 
 def home_redirect(request):
     return render(request, 'citrus_home/index.html')
@@ -112,3 +114,85 @@ def manage_profile(request, id):
 
         response.status_code = 405
         return response
+
+"""
+retrieve github username from github url 
+"""
+def sanitize_git_url(git_url):
+    git_username = re.sub('http://github.com/|https://github.com/|https://www.github.com/|http://www.github.com/', '', git_url)
+    return git_username
+
+"""
+handles get request and list events for the github username 
+Expected: 
+URL:/service/author/{AUTHOR_ID}/github
+reference: https://towardsdatascience.com/build-a-python-crawler-to-get-activity-stream-with-github-api-d1e9f5831d88
+"""
+def get_github_events(request, id):
+    if request.method == 'GET':
+        # look up user by their id, if not exist, return 404 response
+        profile = get_object_or_404(CitrusAuthor, id=id)
+        
+        #sanitize github url to get github username
+        git_username = sanitize_git_url(profile.github)
+
+        response = requests.get('https://api.github.com/users/{username}/events'.format(username=git_username))
+
+        # validate if username exists:
+        if response.status_code == 404:
+            err_response = JsonResponse({
+                "message": "Profile not found"
+            })
+            err_response.status_code = 404
+            return err_response
+
+        # process data to customized json response
+        data = response.json()
+        results = []
+        
+        event_actions = {
+            'WatchEvent': 'starred', 
+            'PushEvent': 'pushed to', 
+            'CreateEvent': "created", 
+            'DeleteEvent':'deleted',
+            'ForkEvent':'forked',
+            'CommitCommentEvent':'committed comment on',
+            'IssueCommentEvent': 'issued comment on',
+            'IssueEvent': 'issued event on',
+            'PullRequestEvent': 'made a pull request on',
+            'PullRequestReviewEvent': 'reviewed a pull request on',
+            'ReleaseEvent': 'made a realease on'
+        }
+        
+        for event in data:
+            if event['type'] in event_actions:
+                name = event['actor']['display_login']
+                action = event_actions[event['type']] 
+                repo = event['repo']['name']
+                time = event['created_at']
+                action_str = {
+                    'type':event['type'],
+                    'name':name,
+                    'action':action,
+                    'repo':repo,
+                    'time':time,
+                }
+                results.append(action_str)
+            if event['type'] == 'ForkEvent':
+                name = event['actor']['display_login']
+                repo = event['repo']['name']  
+                forked_repo = event['payload']['forkee']['full_name'] 
+                time = event['created_at']
+                action_str = {
+                    'type':event['type'],
+                    'name':name,
+                    'action':action,
+                    'forked_repo':forked_repo,
+                    'repo':repo,
+                    'time':time,
+                }
+                results.append(action_str)
+        response = JsonResponse({'events':results})
+        response.status_code = 200
+        return response
+
