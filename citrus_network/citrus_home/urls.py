bc381edf4e22d3ef37dbe58c966d7964b62364ce
leah-is-offline <copeland@ualberta.ca>
2021-02-23 08:38:38 -0800

    reverse naming conventions

--- citrus_network/citrus_home/urls.py
+++ citrus_network/citrus_home/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',views.manage_profile, name='profile_url'),
-  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/github$',views.get_github_events, name='github_url'),
+  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/$',views.manage_profile, name='profile'),
+  url(r'^service/author/(?P<id>[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})/github$',views.get_github_events, name='github'),
   path('', views.login_redirect, name='login_url'),
   path('home/', views.home_redirect, name='home_url'),
   path('register/', views.register_redirect, name='register_url'),
