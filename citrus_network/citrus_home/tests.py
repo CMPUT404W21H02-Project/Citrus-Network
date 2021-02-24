8eeff149f9b396857233091555ba2f8f0c4a763c
leah-is-offline <copeland@ualberta.ca>
2021-02-24 10:24:39 -0800

    model testing, User auth testing, one to one Citrus Author and user child testing, login credential testing

--- citrus_network/citrus_home/tests.py
+++ citrus_network/citrus_home/tests.py
@@ -3,6 +3,7 @@ from citrus_home.models import CitrusAuthor
 from django.contrib.auth.models import User
 import uuid
 from django.test import Client
+from django.contrib.auth import authenticate
 
 
 #https://docs.djangoproject.com/en/3.1/topics/testing/overview/
@@ -14,14 +15,51 @@ from django.test import Client
 
 class CitrusAuthorTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create_user('coolGuy', 'coolGuy@test.com', 'coolpassword')
-        CitrusAuthor.objects.create(type="Author",id=str(uuid.uuid4()), user=user,displayName="coolTestGuy")
+        user = User.objects.create_user(username = 'coolGuy', password='coolpassword')
+        self.testUser = CitrusAuthor.objects.create(type="Author",id=str(uuid.uuid4()), user=user,displayName="coolTestGuy")
+        self.testUser.save()
+
+    def test_citrus_display_name_edit(self):
+        # tests that when a user sets their display name it doesnt default back to the username
+        user =  CitrusAuthor.objects.get(displayName="coolTestGuy")
+        self.assertEqual(user.displayName, "coolTestGuy")
+        self.assertEqual(user.type, "Author")
+    
+    def test_citrus_author_user_one_to_one(self):
+        # tests that the user child object of Citrus author parent object is truly one to one
+        userChild = User.objects.get(username="coolGuy")
+        authorParent =  CitrusAuthor.objects.get(displayName="coolTestGuy")
+        self.assertEqual(authorParent.user, userChild)
+    
+    def tearDown(self):
+        self.testUser.delete()
+
+
+class LoginTestCase(TestCase):
         
-    def test_citrus_author_fields(self):
-        #check that displayName when provided is set to desired name instead of username
-        testUser =  CitrusAuthor.objects.get(displayName="coolTestGuy")
-        self.assertEqual(testUser.displayName, "coolTestGuy")
-        self.assertEqual(testUser.type, "Author")
+    def setUp(self):
+        user = User.objects.create_user('nervousMan', 'man@nervous.com', 'aManThatsNervous')
+        self.nervousTestMan = CitrusAuthor.objects.create(type="Author",id=str(uuid.uuid4()), user=user,displayName="nervousMan")
+        self.nervousTestMan.save()
+
+    def test_correct(self):
+        # tests that authentication passes with correct credentials
+        user = authenticate(username='nervousMan', password='aManThatsNervous')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_incorrect_username(self):
+        # tests that authentication does not pass with incorrect username
+        user = authenticate(username='wrong', password='aManThatsNervous')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_incorrect_password(self):
+        # tests that authentication does not pass with incorrect password
+        user = authenticate(username='nervousMan', password='wrong')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def tearDown(self):
+        self.nervousTestMan.delete()
+
       
 
 
