6ed7cbdb061235772059d2396b85b8c4c09c86e8
leah-is-offline <copeland@ualberta.ca>
2021-02-23 18:34:17 -0800

    simple unit testing

--- citrus_network/citrus_home/tests.py
+++ citrus_network/citrus_home/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from citrus_home.models import CitrusAuthor
+from django.contrib.auth.models import User
+import uuid
+from django.test import Client
 
-# Create your tests here.
+
+#https://docs.djangoproject.com/en/3.1/topics/testing/overview/
+#on a terminal run $ python manage.py test
+
+#TEST DONT PULL FROM YOUR DB, BUT FROM A MOCK DB
+#DJANGO WILL RUN TESTS THAT START WITH THE WORD "TEST"
+
+
+class CitrusAuthorTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('coolGuy', 'coolGuy@test.com', 'coolpassword')
+        CitrusAuthor.objects.create(type="Author",id=str(uuid.uuid4()), user=user,displayName="coolTestGuy")
+        
+    def test_citrus_author_fields(self):
+        #check that displayName when provided is set to desired name instead of username
+        testUser =  CitrusAuthor.objects.get(displayName="coolTestGuy")
+        self.assertEqual(testUser.displayName, "coolTestGuy")
+
+
+       
+
