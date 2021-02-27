984a1639513af644b1338dfcce80b8e27225f45b
fujishig2 <fujishig@ualberta.ca>
2021-02-27 13:31:36 -0700

    added stram front page

--- citrus_network/citrus_home/views.py
+++ citrus_network/citrus_home/views.py
@@ -121,6 +121,107 @@ def home_redirect(request):
         
         return render(request, 'citrus_home/index.html', {'json_list': mock_response})
 
+def stream_redirect(request):
+    if request.method == 'GET':
+        mock_response = [
+            {
+                'type':"post",
+                'title':"A post title about a post about web dev",
+                'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e",
+                'source':"http://lastplaceigotthisfrom.com/posts/yyyyy",
+                'origin':"http://whereitcamefrom.com/posts/zzzzz",
+                'description':"This post discusses stuff -- brief",
+                'contentType':"text/plain",
+                'content':"Þā wæs on burgum Bēowulf Scyldinga, lēof lēod-cyning, longe þrāge folcum gefrǣge (fæder ellor hwearf, aldor of earde), oð þæt him eft onwōc hēah Healfdene; hēold þenden lifde, gamol and gūð-rēow, glæde Scyldingas. Þǣm fēower bearn forð-gerīmed in worold wōcun, weoroda rǣswan, Heorogār and Hrōðgār and Hālga til; hȳrde ic, þat Elan cwēn Ongenþēowes wæs Heaðoscilfinges heals-gebedde. Þā wæs Hrōðgāre here-spēd gyfen, wīges weorð-mynd, þæt him his wine-māgas georne hȳrdon, oð þæt sēo geogoð gewēox, mago-driht micel. Him on mōd bearn, þæt heal-reced hātan wolde, medo-ærn micel men gewyrcean, þone yldo bearn ǣfre gefrūnon, and þǣr on innan eall gedǣlan geongum and ealdum, swylc him god sealde, būton folc-scare and feorum gumena. Þā ic wīde gefrægn weorc gebannan manigre mǣgðe geond þisne middan-geard, folc-stede frætwan. Him on fyrste gelomp ǣdre mid yldum, þæt hit wearð eal gearo, heal-ærna mǣst; scōp him Heort naman, sē þe his wordes geweald wīde hæfde. Hē bēot ne ālēh, bēagas dǣlde, sinc æt symle. Sele hlīfade hēah and horn-gēap: heaðo-wylma bād, lāðan līges; ne wæs hit lenge þā gēn þæt se ecg-hete āðum-swerian 85 æfter wæl-nīðe wæcnan scolde. Þā se ellen-gǣst earfoðlīce þrāge geþolode, sē þe in þȳstrum bād, þæt hē dōgora gehwām drēam gehȳrde hlūdne in healle; þǣr wæs hearpan swēg, swutol sang scopes. Sægde sē þe cūðe frum-sceaft fīra feorran reccan",
+                # the author has an ID where by authors can be disambiguated
+                'author':{
+                    'type':"author",
+                    'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                    'host':"http://127.0.0.1:5454/",
+                    'displayName':"Lara Croft",
+                    'url':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                    'github': "http://github.com/laracroft"
+                },
+                'categories':["web","tutorial"],
+                'count': 1023,
+                'size': 50,
+                'comments':[
+                    {
+                        'type':"comment",
+                        'author':{
+                            'type':"author",
+                            # ID of the Author (UUID)
+                            'id':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                            # url to the authors information
+                            'url':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                            'host':"http://127.0.0.1:5454/",
+                            "displayName":"Greg Johnson",
+                            # HATEOS url for Github API
+                            'github': "http://github.com/gjohnson"
+                        },
+                        'comment':"Sick Olde English",
+                        'contentType':"text/markdown",
+                        # ISO 8601 TIMESTAMP
+                        'published':"2015-03-09T13:07:04+00:00",
+                        # ID of the Comment (UUID)
+                        'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
+                    }
+                ],
+                'published':"2015-03-09T13:07:04+00:00",
+                'visibility':"PUBLIC",
+                'unlisted': False,
+            },
+            {
+                'type':"post",
+                'title':"How to work on Django",
+                'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/764efa883dda1e11db47671c4a3bbd9e",
+                'source':"http://lastplaceigotthisfrom.com/posts/yyyyy",
+                'origin':"http://heroku.com/posts/aaaa",
+                'description':"This post discusses stuff -- brief",
+                'contentType':"text/plain",
+                'content':"Þā wæs on burgum Bēowulf Scyldinga, lēof lēod-cyning, longe þrāge folcum gefrǣge (fæder ellor hwearf, aldor of earde), oð þæt him eft onwōc hēah Healfdene; hēold þenden lifde, gamol and gūð-rēow, glæde Scyldingas. Þǣm fēower bearn forð-gerīmed in worold wōcun, weoroda rǣswan, Heorogār and Hrōðgār and Hālga til; hȳrde ic, þat Elan cwēn Ongenþēowes wæs Heaðoscilfinges heals-gebedde. Þā wæs Hrōðgāre here-spēd gyfen, wīges weorð-mynd, þæt him his wine-māgas georne hȳrdon, oð þæt sēo geogoð gewēox, mago-driht micel. Him on mōd bearn, þæt heal-reced hātan wolde, medo-ærn micel men gewyrcean, þone yldo bearn ǣfre gefrūnon, and þǣr on innan eall gedǣlan geongum and ealdum, swylc him god sealde, būton folc-scare and feorum gumena. Þā ic wīde gefrægn weorc gebannan manigre mǣgðe geond þisne middan-geard, folc-stede frætwan. Him on fyrste gelomp ǣdre mid yldum, þæt hit wearð eal gearo, heal-ærna mǣst; scōp him Heort naman, sē þe his wordes geweald wīde hæfde. Hē bēot ne ālēh, bēagas dǣlde, sinc æt symle. Sele hlīfade hēah and horn-gēap: heaðo-wylma bād, lāðan līges; ne wæs hit lenge þā gēn þæt se ecg-hete āðum-swerian 85 æfter wæl-nīðe wæcnan scolde. Þā se ellen-gǣst earfoðlīce þrāge geþolode, sē þe in þȳstrum bād, þæt hē dōgora gehwām drēam gehȳrde hlūdne in healle; þǣr wæs hearpan swēg, swutol sang scopes. Sægde sē þe cūðe frum-sceaft fīra feorran reccan",
+                # the author has an ID where by authors can be disambiguated
+                'author':{
+                    'type':"author",
+                    'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                    'host':"http://127.0.0.1:5454/",
+                    'displayName':"Craft Smith",
+                    'url':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                    'github': "http://github.com/laracroft"
+                },
+                'categories':["web","tutorial"],
+                'count': 1023,
+                'size': 50,
+                'comments':[
+                    {
+                        'type':"comment",
+                        'author':{
+                            'type':"author",
+                            # ID of the Author (UUID)
+                            'id':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                            # url to the authors information
+                            'url':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                            'host':"http://127.0.0.1:5454/",
+                            "displayName":"Greg Johnson",
+                            # HATEOS url for Github API
+                            'github': "http://github.com/gjohnson"
+                        },
+                        'comment':"Sick Olde English",
+                        'contentType':"text/markdown",
+                        # ISO 8601 TIMESTAMP
+                        'published':"2015-03-09T13:07:04+00:00",
+                        # ID of the Comment (UUID)
+                        'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
+                    }
+                ],
+                'published':"2012-03-09T13:07:04+00:00",
+                'visibility':"PUBLIC",
+                'unlisted': False,
+            },
+        ]
+        
+        return render(request, 'citrus_home/stream.html', {'json_list': mock_response})
+
 def login_redirect(request):
     if request.method == "POST":
         print("here")
