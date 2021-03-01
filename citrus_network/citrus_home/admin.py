bfc5e0445f12c8e7fdd6aa724b79cbbfffe8134e
Nhan <nhnguyen@ualberta.ca>
2021-02-28 17:56:17 -0700

    edited friends and followers models

--- citrus_network/citrus_home/admin.py
+++ citrus_network/citrus_home/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import CitrusAuthor
+from .models import CitrusAuthor,Friend,Follower
 
 
 class CitrusAuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'type', 'user', 'host', 'displayName', 'github',)
 
-admin.site.register(CitrusAuthor, CitrusAuthorAdmin)
+class FriendAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'friends_uuid',)
+
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'followers_uuid',)
+
+admin.site.register(CitrusAuthor, CitrusAuthorAdmin,)
+
+admin.site.register(Friend, FriendAdmin,)
+
+admin.site.register(Follower, FollowerAdmin,)
--- citrus_network/citrus_home/models.py
+++ citrus_network/citrus_home/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import User
+from django.core.validators import int_list_validator
 
 
 class CitrusAuthor(models.Model):
@@ -10,3 +11,13 @@ class CitrusAuthor(models.Model):
     host            = models.CharField(max_length=200, default="http://localhost:8000/")
     displayName     = models.CharField(max_length=300, default=f"{str(user)}")
     github          = models.CharField(max_length=300, default="", null=True)
+
+
+# https://stackoverflow.com/questions/1429293/storing-an-integer-array-in-a-django-database
+class Friend(models.Model):
+    uuid              = models.ForeignKey(CitrusAuthor, on_delete=models.CASCADE)
+    friends_uuid    = models.TextField(validators=[int_list_validator])
+
+class Follower(models.Model):
+    uuid              = models.ForeignKey(CitrusAuthor, on_delete=models.CASCADE)
+    followers_uuid  = models.TextField(validators=[int_list_validator])
