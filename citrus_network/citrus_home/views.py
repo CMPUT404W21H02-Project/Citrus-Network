e0edaec4550226a3e44e8922bb9694f5e058fd11
fujishig2 <fujishig@ualberta.ca>
2021-03-01 00:08:14 -0700

    finishing touches. Front end view made up. Still missing ajax calls.

--- citrus_network/citrus_home/views.py
+++ citrus_network/citrus_home/views.py
@@ -123,6 +123,46 @@ def post_redirect(request):
                     'published':"2015-03-09T13:07:04+00:00",
                     # ID of the Comment (UUID)
                     'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
+                },
+                {
+                    'type':"comment",
+                    'author':{
+                        'type':"author",
+                        # ID of the Author (UUID)
+                        'id':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                        # url to the authors information
+                        'url':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                        'host':"http://127.0.0.1:5454/",
+                        "displayName":"Greg Johnson",
+                        # HATEOS url for Github API
+                        'github': "http://github.com/gjohnson"
+                    },
+                    'comment':"Sick Olde English",
+                    'contentType':"text/markdown",
+                    # ISO 8601 TIMESTAMP
+                    'published':"2015-03-09T13:07:04+00:00",
+                    # ID of the Comment (UUID)
+                    'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
+                }, 
+                {
+                    'type':"comment",
+                    'author':{
+                        'type':"author",
+                        # ID of the Author (UUID)
+                        'id':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                        # url to the authors information
+                        'url':"http://127.0.0.1:5454/author/1d698d25ff008f7538453c120f581471",
+                        'host':"http://127.0.0.1:5454/",
+                        "displayName":"Greg Johnson",
+                        # HATEOS url for Github API
+                        'github': "http://github.com/gjohnson"
+                    },
+                    'comment':"Sick Olde English",
+                    'contentType':"text/markdown",
+                    # ISO 8601 TIMESTAMP
+                    'published':"2015-03-09T13:07:04+00:00",
+                    # ID of the Comment (UUID)
+                    'id':"http://127.0.0.1:5454/author/9de17f29c12e8f97bcbbd34cc908f1baba40658e/posts/de305d54-75b4-431b-adb2-eb6b9e546013/comments/f6255bb01c648fe967714d52a89e8e9c",
                 }
             ],
             'published':"2015-03-09T13:07:04+00:00",
