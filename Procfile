c5ca535976757269295e62a70cc977cdabd6f90b
leah-is-offline <copeland@ualberta.ca>
2021-03-05 16:02:06 -0800

    procile, README.md , settings

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-release: python manage.py migrate
-web: gunicorn citrus_home.wsgi --log-file -
+web: gunicorn citrus_network.wsgi 
--- citrus_network/settings.py
+++ citrus_network/settings.py
@@ -1,3 +1,4 @@
+import django_on_heroku 
 """
 Django settings for citrus_network project.
 
@@ -9,7 +10,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_on_heroku # top of the file
+# top of the file
 from pathlib import Path
 import os
 
